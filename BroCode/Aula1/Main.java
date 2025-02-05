c9911059e77462db2527532fb8c88ac2d12dfc3c
LeonardoKaue536 <165850020+LeonardoKaue536@users.noreply.github.com>
2025-02-05 17:04:45 -0300

    Brocode Aula 1 e 2
    Fiz as Aulas 1 e 2

---
+++ BroCode/Aula1/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    
+    public static void main(String[] args){
+
+        //Dizer Hello World
+
+        System.out.print("Hello World!");
+        System.out.print("Eu amo Java!");
+        System.out.print("Porque é bom!");
+        
+    }
+
+}
---
+++ BroCode/Aula2/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args){
+        
+        int age = 19;
+        System.out.println("Leonardo is "+ age +" years old");
+        
+        //Strings
+
+        String name = "Leonardo";
+        String food = "Lasanha";
+        String email = "FakeEmail@gmail.com";
+        String car = "Mustang";
+
+        System.out.println("Hello " + name);
+        System.out.println("My favorite food is " + food);
+        System.out.println("Your email is " + email);
+        System.out.println("And your car is the" + car);
+
+      
+    }
+}
