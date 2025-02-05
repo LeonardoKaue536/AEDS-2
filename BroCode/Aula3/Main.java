2500c77220890c9d832d79a89025dda1837cc547
Leonardo Kauê <leonardokaue536@gmail.com>
2025-02-05 17:51:54 -0300

    Aula 3 finalizada
    Terminei a aula 3

--- BroCode/Aula3/Main.java
+++ BroCode/Aula3/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();//Para eu limpar o Buffer      
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your height(in meters): ");
+        double height = scanner.nextDouble();
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("You are " + height + "m²");
+
+        scanner.close();
     }
 }
